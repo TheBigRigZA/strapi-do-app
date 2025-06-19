69ed4d6951b9812bac8916a162f779a3d289c8f6
James <james@mediabox.co.za>
2025-06-19 14:35:14 +0200

    Configure Strapi for Supabase and Medusa integration

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,9 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  url: env('PUBLIC_URL', ''),
+  proxy: env.bool('IS_PROXIED', true),
 });
